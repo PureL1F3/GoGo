83ac31ce8fa9978918ea406d361b3b40636717e9
PureL1F3 <triggerbunny@LazEs-MacBook-Pro.local>
2014-06-04 02:44:40 -0400

    Initial Commit

---
+++ GoGo/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  GoGo
+//
+//  Created by LazE on 6/4/14.
+//  Copyright (c) 2014 BabyJeff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
